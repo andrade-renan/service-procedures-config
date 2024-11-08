a830cd16e1a93336a0100a79422460e69e56716d
Renan <renanmunizandrade@hotmail.com>
2024-11-08 15:42:51 -0300

    Concluded

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import pandas as pd
 import json
 
-with open('procedures.json', 'r') as file:
+# Criar uma base de dados com os procedimentos e seus valores, e uma estrutura que relaciona serviços e procedimentos
+with open('data\procedures.json', 'r', encoding='utf-8') as file:
     procedures = json.load(file)
 
-with open('services.json', 'r') as file:
+# Serviços e os procedimentos que os compõem
+with open('data\services.json', 'r', encoding='utf-8') as file:
     services = json.load(file)
-    
 
-print(procedures)
+
+# Calculando o total e comisão para cada serviço
+results = {}
+for service, steps in services.items():
+    total_cost = sum([procedures[step][0] for step in steps])
+    total_commission = sum([procedures[step][1] for step in steps])
+    results[service] = {"Valor Total": total_cost, "Comissão": total_commission, "Porcentagem de Comissão": total_commission/total_cost}
+
+
+# Criar uma planilha usando pandas
+df_services = pd.DataFrame.from_dict(results, orient='index')
+
+# Salvar em um arquivo Excel para fácil edição
+file_path = 'data\Servicos_Etapas.xlsx'
+with pd.ExcelWriter(file_path) as writer:
+    df_services.to_excel(writer, sheet_name='Valores_Serviços')
+
+df_services, file_path
