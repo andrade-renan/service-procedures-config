0725fca1b579d2b18875ecf5562ac21b5e13ced8
Renan <renanmunizandrade@hotmail.com>
2024-11-08 16:10:50 -0300

    Include time

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ with open('data\services.json', 'r', encoding='utf-8') as file:
 # Calculando o total e comisão para cada serviço
 results = {}
 for service, steps in services.items():
-    total_cost = sum([procedures[step][0] for step in steps])
-    total_commission = sum([procedures[step][1] for step in steps])
-    results[service] = {"Valor Total": total_cost, "Comissão": total_commission, "Porcentagem de Comissão": total_commission/total_cost}
+    total_cost = sum([procedures[step]['Valor'] for step in steps])
+    total_commission = sum([procedures[step]['Repasse'] for step in steps])
+    total_time = sum([procedures[step]['Tempo'] for step in steps])
+    results[service] = {"Valor Total": total_cost, "Comissão": total_commission, "Porcentagem de Comissão": total_commission/total_cost, "Tempo": total_time}
 
 
 # Criar uma planilha usando pandas
